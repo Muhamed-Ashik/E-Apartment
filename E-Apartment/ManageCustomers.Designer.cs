a6aee0b30365df1565268424bc281df1b04f5eb6
Muhamed-Ashik <muhamedashikofficial@gmail.com>
2023-01-15 17:37:44 +0530

    New pages are added

--- E-Apartment/ManageCustomers.Designer.cs
+++ E-Apartment/ManageCustomers.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace E_Apartment
 {
-    partial class ManageApartments
+    partial class ManageCustomers
     {
         /// <summary>
         /// Required designer variable.
@@ -32,31 +32,31 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.label1.Location = new System.Drawing.Point(377, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(386, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(377, 46);
+            this.label1.Size = new System.Drawing.Size(250, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Manage Apartments";
+            this.label1.Text = "Manage Customers";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(215, 531);
+            this.button1.Location = new System.Drawing.Point(121, 503);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Insert ";
+            this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(519, 286);
+            this.button2.Location = new System.Drawing.Point(234, 503);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -65,36 +65,36 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(608, 456);
+            this.button3.Location = new System.Drawing.Point(362, 503);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // button4
             // 
-            this.button5.Location = new System.Drawing.Point(734, 358);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Clear";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(499, 503);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear Text";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // ManageApartments
+            // ManageCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1241, 632);
-            this.Controls.Add(this.button5);
+            this.ClientSize = new System.Drawing.Size(1195, 646);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
-            this.Name = "ManageApartments";
+            this.Name = "ManageCustomers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Manage Apartments";
+            this.Text = "ManageCustomers";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +106,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
     }
 }

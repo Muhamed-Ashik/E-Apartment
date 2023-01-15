4a9d232351535a8a8363418c2208a3904f87d54a
Muhamed-Ashik <muhamedashikofficial@gmail.com>
2023-01-15 11:36:02 +0530

    New Forms are Added

--- E-Apartment/Program.cs
+++ E-Apartment/Program.cs
@@ -16,7 +16,7 @@ namespace E_Apartment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AdminLogin());
         }
     }
 }
